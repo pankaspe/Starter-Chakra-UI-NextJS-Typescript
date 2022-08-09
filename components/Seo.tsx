a00c217ef5d46371276069f0114112c44346f12e
pankaspe <mez.tnt@gmail.com>
2022-08-09 11:51:29 +0200

    first deploy

---
+++ components/Seo.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import websiteConfig from '../lib/config/website';
+
+export interface Seo {
+   title: string,
+   description: string,
+   hero?: string,
+}
+
+const Seo: React.FC<Seo> = ({ title, description }) => {
+   return (
+     <>
+      <Head>
+        <title>{websiteConfig.meta.title} - {title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     </>
+   )
+ }
+ 
+ export default Seo;
